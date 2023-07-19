89303c6bd74a393664d2cd8a4bae5376a6aed776
Ardeshir.Talaei <ardeshir.talaei@iaac.net>
2023-07-19 16:23:59 +0200

    Add scatter and pie charts

--- rta-app/src/components/analytical/HoleOffsetsScatter.vue
+++ rta-app/src/components/analytical/HoleOffsetsScatter.vue
@@ -12,7 +12,7 @@
 <script setup>
 import { use } from "echarts/core";
 import { SVGRenderer } from "echarts/renderers";
-import { PieChart } from "echarts/charts";
+import { ScatterChart } from "echarts/charts";
 import {
   TitleComponent,
   TooltipComponent,
@@ -21,7 +21,13 @@ import {
 import VChart, { THEME_KEY } from "vue-echarts";
 import { ref, provide } from "vue";
 
-use([SVGRenderer, PieChart, TitleComponent, TooltipComponent, LegendComponent]);
+use([
+  SVGRenderer,
+  ScatterChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+]);
 
 provide(THEME_KEY, "dark");
 
@@ -41,23 +47,42 @@ const option = ref({
     top: "bottom",
     data: ["Svetlana", "Mercedes"],
   },
+  xAxis: {},
+  yAxis: {},
   series: [
     {
-      name: "Parts Assembled",
-      type: "pie",
-      radius: ["30%", "60%"],
-      center: ["50%", "50%"],
+      symbolSize: 20,
       data: [
-        { value: 35, name: "Svetlana" },
-        { value: 31, name: "Mercedes" },
+        [11.5, 7.2],
+        [3.03, 4.23],
+        [12.2, 7.83],
+        [2.02, 4.47],
+        [1.05, 3.33],
+        [4.05, 4.96],
+        [6.03, 7.24],
+        [12.0, 6.26],
+        [12.0, 8.84],
+        [7.08, 5.82],
+        [5.02, 5.68],
       ],
-      emphasis: {
-        itemStyle: {
-          shadowBlur: 10,
-          shadowOffsetX: 0,
-          shadowColor: "rgba(0, 0, 0, 0.5)",
-        },
-      },
+      type: "scatter",
+    },
+    {
+      symbolSize: 20,
+      data: [
+        [10.0, 8.04],
+        [8.07, 6.95],
+        [13.0, 7.58],
+        [9.05, 8.81],
+        [11.0, 8.33],
+        [14.0, 7.66],
+        [13.4, 6.81],
+        [10.0, 6.33],
+        [14.0, 8.96],
+        [12.5, 6.82],
+        [9.15, 7.2],
+      ],
+      type: "scatter",
     },
   ],
 });
@@ -65,6 +90,6 @@ const option = ref({
 
 <style scoped>
 /* .chart {
-  height: 100px;
-} */
+    height: 100px;
+  } */
 </style>
--- rta-app/src/views/AssembliesView.vue
+++ rta-app/src/views/AssembliesView.vue
@@ -1,11 +1,21 @@
 <script setup>
 import AssembliesList from "../components/assemblies/AssembliesList.vue";
+import AgentWorkloads from "../components/analytical/AgentWorkloads.vue";
+import HoleOffsetsScatter from "../components/analytical/HoleOffsetsScatter.vue";
 </script>
 
 <template>
   <div class="row">
     <div class="col-lg-4 col-md-6 col-sm-12">
       <AssembliesList />
+    </div>
+
+    <div class="col-lg-3 col-md-4">
+      <AgentWorkloads class="m-1"/>
+    </div>
+
+    <div class="col-lg-6 col-md-9">
+      <HoleOffsetsScatter class="m-1"/>
     </div>
   </div>
   <!-- <main>
