e82fb0236f0d9437b032465648117513aafe6b6d
Ardeshir Talaei <ardeshir.talaei@iaac.net>
2023-07-27 15:30:00 +0200

    Styling HomeView with nested Router

--- rta-app/src/views/HomeView.vue
+++ rta-app/src/views/HomeView.vue
@@ -3,10 +3,13 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <div class="row h-100">
-    <div class="col-12 col-lg-6 bg_image">
-      <div class="h-25 d-flex flex-column justify-content-end">
-        <div class="border-end">
+  <div class="row bg_image" style="background-color: black; min-height: 100%">
+    <div class="col-12 col-lg-6">
+      <div
+        class="d-flex justify-content-center align-items-end"
+        style="height: 20vh"
+      >
+        <div class="border-end h-50 w-100">
           <p class="display-1 fw-lighter text-center">RECONWOOD</p>
           <p class="fw-light text-center">RECONFIGURABLE TIMBER ASSEMBLIES</p>
         </div>
@@ -14,10 +17,15 @@ import { RouterLink, RouterView } from "vue-router";
     </div>
 
     <div
-      class="col-sm-12 col-lg-6 flex-fill d-flex flex-column justify-content-center"
-      style="background-color: black"
+      class="col-12 col-lg-6 flex-fill d-flex flex-column justify-content-start"
     >
-      <router-view />
+        <div class="d-none d-lg-block" style="height: 10vh"></div>
+      <div
+        class="border-start p-3"
+        style="background-color: rgba(0, 0, 0, 0.75)"
+      >
+        <router-view />
+      </div>
     </div>
   </div>
 </template>
