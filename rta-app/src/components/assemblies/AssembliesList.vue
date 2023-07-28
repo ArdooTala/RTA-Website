c3fb15609bee55327e7c6d67d019b4be85f8c3b8
Ardeshir Talaei <ardeshir.talaei@iaac.net>
2023-07-28 11:05:30 +0200

    Fix Accordion BG Color

--- rta-app/src/components/assemblies/AssembliesList.vue
+++ rta-app/src/components/assemblies/AssembliesList.vue
@@ -27,54 +27,59 @@ export default {
 </script>
 
 <template>
-  <button
-    class="btn btn-dark dropdown-toggle border w-100 mt-2 d-block d-md-none"
-    type="button"
-    data-bs-toggle="collapse"
-    data-bs-target="#assembliesCollapseTarget"
-  >
-    ASSEMBLIES
-  </button>
-  <div class="collapse show" id="assembliesCollapseTarget">
-    <div
-      class="accordion accordion-flush rounded-4 overflow-hidden"
-      id="assembliesAccordion"
+  <div class="mt-2">
+    <button
+      class="btn btn-dark dropdown-toggle border w-100 mt-2 d-block d-md-none"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#assembliesCollapseTarget"
     >
-      <div v-for="ass in assemblies" class="accordion-item p-2 border-bottom">
-        <div class="accordion-header justify-content-between">
-          <button
-            type="button"
-            class="btn w-100 me-auto d-flex align-items-center"
-            data-bs-toggle="collapse"
-            :data-bs-target="'#' + ass._id"
-          >
-            <!-- <EcosystemIcon class="me-2" /> -->
-            <h5 class="p-2 m-0 me-auto">{{ ass._id }}</h5>
-            <span class="badge rounded-pill text-bg-light">{{
-              ass.count
-            }}</span>
-          </button>
-        </div>
-
+      ASSEMBLIES
+    </button>
+    <div class="collapse show" id="assembliesCollapseTarget">
+      <div
+        class="accordion accordion-flush overflow-hidden"
+        id="assembliesAccordion"
+      >
         <div
-          :id="ass._id"
-          class="accordion-collapse collapse bg-transparent"
-          data-bs-parent="#assembliesAccordion"
+          v-for="ass in assemblies"
+          class="accordion-item p-2 border-bottom bg-transparent"
         >
-          <div class="accordion-body">
-            <AssemblyCard class="">
-              <template #icon><EcosystemIcon /></template>
-              <template #start>{{ ass._id }}</template>
-              <template #end>{{ ass._id }}</template>
-              <template #parts_count>{{ ass.count }}</template>
-            </AssemblyCard>
-
-            <router-link
-              :to="'/assemblies/' + ass._id"
+          <div class="accordion-header justify-content-between">
+            <button
               type="button"
-              class="btn btn-dark border w-100 mt-4"
-              >Open in Dashboard</router-link
+              class="btn w-100 me-auto d-flex align-items-center"
+              data-bs-toggle="collapse"
+              :data-bs-target="'#' + ass._id"
             >
+              <!-- <EcosystemIcon class="me-2" /> -->
+              <h5 class="p-2 m-0 me-auto">{{ ass._id }}</h5>
+              <span class="badge rounded-pill text-bg-light">{{
+                ass.count
+              }}</span>
+            </button>
+          </div>
+
+          <div
+            :id="ass._id"
+            class="accordion-collapse collapse bg-transparent"
+            data-bs-parent="#assembliesAccordion"
+          >
+            <div class="accordion-body">
+              <AssemblyCard class="">
+                <template #icon><EcosystemIcon /></template>
+                <template #start>{{ ass._id }}</template>
+                <template #end>{{ ass._id }}</template>
+                <template #parts_count>{{ ass.count }}</template>
+              </AssemblyCard>
+
+              <router-link
+                :to="'/assemblies/' + ass._id"
+                type="button"
+                class="btn btn-dark border w-100 mt-4"
+                >Open in Dashboard</router-link
+              >
+            </div>
           </div>
         </div>
       </div>
