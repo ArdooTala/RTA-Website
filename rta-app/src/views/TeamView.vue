19b1bc45212f16b6661380d533ecc38ece3367b7
Ardeshir Talaei <ardeshir.talaei@iaac.net>
2023-08-01 15:08:51 +0200

    Update TeamView

--- rta-app/src/views/TeamView.vue
+++ rta-app/src/views/TeamView.vue
@@ -1,12 +1,55 @@
 <template>
-  <div class="team row d-flex justify-content-around">
-    <div class="col-sm-12 col-md-6 col-lg-4 row" data-bs-theme="dark">
-      <h4>Svetlana</h4>
-      <img src="../assets/ur10e-4x3.png" alt="">
+  <div class="team row justify-content-around">
+    <div class="col-sm-12 col-md-6 col-lg-4 row">
+      <h4>SVETLANA</h4>
+      <img src="../assets/Merc.jpg" alt="" />
+      <p>Codename: The Bitch</p>
+      <p>
+        Is an experienced former russian field-agent, expertised in handling the
+        dirty works.
+      </p>
     </div>
     <div class="col-sm-12 col-md-6 col-lg-4 row">
-      <h4>Mercedes</h4>
-      <img src="../assets/ur10e-4x3.png" alt="">
+      <h4>MERCEDES</h4>
+      <img src="../assets/Merc.jpg" alt="" />
+      <p>Codename: The Vice Bitch</p>
+      <p>
+        Denies any association to Russia and is the second-in-command in the
+        forefront of the fearsom underground empire. Acts as the (red) right
+        hand to Svetlana in delivering the dirty works.
+      </p>
+    </div>
+    <div class="col-sm-12 col-md-6 col-lg-4 row">
+      <h4>HOLOLENS</h4>
+      <img src="../assets/hololens.png" alt="" />
+      <p>Codename: The Parasite</p>
+      <p>
+        Neutralizes its target by wrapping around its head and taking control of
+        their brain by tunneling through their visual cortex. From a third
+        person's view, it's victims seem to be delusional, trying to poke
+        invisible objects mid air. Hololens then utilizes the victim's motoring
+        skills to its own benefits.
+      </p>
+    </div>
+    <div class="col-sm-12 col-md-6 col-lg-4 row">
+      <h4>ROS</h4>
+      <img src="../assets/Ross.jpg" alt="" />
+      <p>Codename: The Conductor</p>
+      <p>
+        Not to be confused with Dr. Ross Geller despite the similar appearances.
+        ROS receives command from an unknown higher source and is responsible
+        for orchestrating the operations.
+      </p>
+    </div>
+    <div class="col-sm-12 col-md-6 col-lg-4 row">
+      <h4>GRASSHOPPER</h4>
+      <img src="../assets/GH.webp" alt="" />
+      <p>Codename: The Choreographer</p>
+      <p>
+        The master-mind behind every operation of the underground empire. Acting
+        undercover from behind a leaf, as evident in the image, the true
+        identity of Grasshopper has always remained a mystery.
+      </p>
     </div>
   </div>
 </template>
