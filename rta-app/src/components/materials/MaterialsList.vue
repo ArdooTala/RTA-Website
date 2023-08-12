ea8a9652ef7f1cfcd4a38b1242f303520a4e1d1f
Ardeshir Talaei <ardeshir.talaei@iaac.net>
2023-08-12 17:20:09 +0200

    Add search to MaterialsList

--- rta-app/src/components/materials/MaterialsList.vue
+++ rta-app/src/components/materials/MaterialsList.vue
@@ -1,5 +1,4 @@
 <script setup>
-// import MaterialCard from "./MaterialCard.vue";
 import { RouterLink } from "vue-router";
 import { ref, watch } from "vue";
 
@@ -33,6 +32,15 @@ function getMatByTypes() {
         .then((jsonRes) => {
             //   console.log("Filtered");
             //   console.log(jsonRes);
+            jsonRes.sort((a, b) => {
+                if (a.name < b.name) {
+                    return 1;
+                }
+                if (a.name > b.name) {
+                    return -1;
+                }
+                return 0;
+            })
             filtered_blocks.value = jsonRes;
         });
 }
@@ -81,23 +89,34 @@ watch(checkedNames, () => {
                     <div class="offcanvas-body">
                         <div class="row">
                             <div class="input-group">
-                                <input type="text" class="form-control" v-model="typeID" placeholder="Tag" />
-                                <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="">
-                                    Search
+
+                                <input type="text" class="form-control" list="datalistOptions" v-model="typeID"
+                                    placeholder="Type" />
+                                <datalist id="datalistOptions">
+                                    <option v-for="blk_type in blk_types" :value="blk_type._id" />
+                                </datalist>
+
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon3"
+                                    @click="checkedNames = []">
+                                    Clear Selection
                                 </button>
                             </div>
 
                             <div class="col-12">
-                                <div class="form-check border-bottom" v-for="blk_type in blk_types">
-                                    <input class="form-check-input" type="checkbox" :id="blk_type._id" :value="blk_type._id"
-                                        v-model="checkedNames" />
-                                    <label class="form-check-label d-flex justify-content-between mt-2" :for="blk_type._id">
-                                        <p>{{ blk_type._id }}</p>
-                                        <span class="badge rounded-pill text-bg-light align-self-start">
-                                            {{ blk_type.count }}
-                                        </span>
-                                    </label>
-                                </div>
+                                <template v-for="blk_type in blk_types">
+                                    <template v-if="blk_type._id.includes(typeID)">
+                                        <input class="btn-check" type="checkbox" :id="blk_type._id" :value="blk_type._id"
+                                            v-model="checkedNames" />
+                                        <label class="btn btn-outline-dark d-flex justify-content-between my-1"
+                                            :for="blk_type._id">
+                                            {{ blk_type._id }}
+                                            <span class="badge rounded-pill text-bg-light align-self-center">
+                                                {{ blk_type.count }}
+                                            </span>
+                                        </label>
+
+                                    </template>
+                                </template>
                             </div>
                         </div>
                     </div>
@@ -119,26 +138,28 @@ watch(checkedNames, () => {
                     </div>
 
                     <div class="accordion accordion-flush overflow-hidden" id="assembliesAccordion">
-                        <div v-for="mat_block in filtered_blocks" class="accordion-item p-2 border-bottom bg-transparent">
-                            <div class="accordion-header justify-content-between">
-                                <button type="button" class="btn w-100 me-auto d-flex align-items-center"
-                                    data-bs-toggle="collapse" :data-bs-target="'#' + mat_block._id">
-                                    <h5 class="p-2 m-0 me-auto">{{ mat_block.name }}</h5>
-                                    <span class="badge rounded-pill text-bg-light">
-                                        {{ mat_block.type }}
-                                    </span>
-                                </button>
-                            </div>
+                        <template v-for="mat_block in filtered_blocks">
+                            <div class="accordion-item p-2 border-bottom bg-transparent">
+                                <div class="accordion-header justify-content-between">
+                                    <button type="button" class="btn w-100 me-auto d-flex align-items-center"
+                                        data-bs-toggle="collapse" :data-bs-target="'#' + mat_block._id">
+                                        <h5 class="p-2 m-0 me-auto">{{ mat_block.name }}</h5>
+                                        <span class="badge rounded-pill text-bg-light">
+                                            {{ mat_block.type }}
+                                        </span>
+                                    </button>
+                                </div>
 
-                            <div :id="mat_block._id" class="accordion-collapse collapse bg-transparent"
-                                data-bs-parent="#assembliesAccordion">
-                                <div class="accordion-body">
-                                    <!-- <p>{{ mat_block || "-" }}</p> -->
-                                    <router-link :to="'/mps/' + mat_block.name" type="button"
-                                        class="btn btn-dark border w-100 mt-4">Open in Dashboard</router-link>
+                                <div :id="mat_block._id" class="accordion-collapse collapse bg-transparent"
+                                    data-bs-parent="#assembliesAccordion">
+                                    <div class="accordion-body">
+                                        <!-- <p>{{ mat_block || "-" }}</p> -->
+                                        <router-link :to="'/mps/' + mat_block.name" type="button"
+                                            class="btn btn-dark border w-100 mt-4">Open in Dashboard</router-link>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </template>
                     </div>
                 </div>
             </div>
