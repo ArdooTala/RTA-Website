30ce64e8d3984fe7e35a59daf0b3ad98f4b33286
Ardeshir Talaei <ardeshir.talaei@iaac.net>
2023-12-04 17:45:01 +0100

    Add Speckle Support

--- server/routes/speckle.mjs
+++ server/routes/speckle.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import { request, gql, GraphQLClient } from "graphql-request";
+import db from "../db/conn.mjs";
 
 const graphQLClient = new GraphQLClient("https://speckle.xyz/graphql", {
   headers: {
@@ -18,13 +19,13 @@ router.get("/", async (req, res) => {
         name
         object(id: "ca3275b79e92a383ecddc2de7da46fc3") {
           id
-          children(query: $Q1, select: [ "id", "ClusterName" ], limit: 1000) {
+          children(query: $Q1, select: ["id", "ClusterName"], limit: 1000) {
             objects {
               data
               sub_clusters: children(
                 query: $Q1
                 depth: 2
-                select: [ "id", "ClusterName" ]
+                select: ["id", "ClusterName"]
                 limit: 1000
               ) {
                 objects {
@@ -34,7 +35,7 @@ router.get("/", async (req, res) => {
               parts: children(
                 query: $Q2
                 depth: 2
-                select: [ "id", "PartName" ]
+                select: ["id", "PartName"]
                 limit: 1000
               ) {
                 objects {
@@ -57,17 +58,60 @@ router.get("/", async (req, res) => {
         operator: "!=",
       },
     ],
-    "Q2": [
-        {
-          "field": "PartName",
-          "value": "",
-          "operator": "!="
-        }
-      ]
+    Q2: [
+      {
+        field: "PartName",
+        value: "",
+        operator: "!=",
+      },
+    ],
   };
 
   const data = await graphQLClient.request(query, variables);
-  
+
+  res.send(data.stream).status(200);
+});
+
+// Get a list of the Parts in an assembly.
+router.get("/:cluster_name", async (req, res) => {
+  let collection = await db.collection("clusters");
+
+  // Query for a movie that has the title 'Back to the Future'
+  const mongoQuery = { name: req.params.cluster_name };
+  const speckleID = await collection.findOne(mongoQuery);
+
+  if (!speckleID) {
+    res.send({}).status(400);
+    return;
+  }
+
+  const query = gql`
+    query AssemblyParts(
+      $streamId: String!
+      $objectId: String!
+      $select: [String]
+    ) {
+      stream(id: $streamId) {
+        object(id: $objectId) {
+          children(select: $select) {
+            objects {
+              id
+              data
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const variables = {
+    streamId: speckleID.speckle.stream_id,
+    objectId: speckleID.speckle.referenced_object,
+    select: ["PartName", "PartType", "\\@NutsLoc", "BoltsGeo"],
+  };
+
+  const data = await graphQLClient.request(query, variables);
+
   res.send(data.stream).status(200);
 });
 
